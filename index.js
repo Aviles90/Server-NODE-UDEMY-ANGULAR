883df310681faa8711234ced78d72485a370d87f
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-08 14:02:42 -0600

    configuracion de express y primer GET

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola ');
+const express = require('express');
+
+//crear el servidor de express
+const app = express();
+
+//GET
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Todo salio bien',
+        uid: 123
+    })
+})
+
+app.listen( 4000, () =>{
+    console.log(`Servidor corriendo en puerto ${4000}`);
+})
