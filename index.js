0205387359daba69645382974afc88b5cdbf407e
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-08 18:41:09 -0600

    conexion a la bd

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const {dbConnection} = require('./db/config')
 require('dotenv').config();
 
 //crear el servidor de express
 const app = express();
+
+//BASE DE DATOS
+dbConnection();
 
 //DIRECTORIO PUBLICO
 app.use(express.static('public'));
