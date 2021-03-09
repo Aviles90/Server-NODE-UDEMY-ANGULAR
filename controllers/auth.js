dba5332629dd139f91a912fb8c306e3320e8335c
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-08 19:30:34 -0600

    Crear usuario en BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,45 @@
 const{ response } = require('express');
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req,res = response) => { //response lo usaremos como un tipado, es comp`letamente opcional
-    console.log( req.body );
+const crearUsuario = async(req,res = response) => { //response lo usaremos como un tipado, es comp`letamente opcional
+    // console.log( req.body );
     const {email, name, password} = req.body; //forma de capturar el body
-    console.log(email, name, password);
+    // console.log(email, name, password);
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+    try {
+         // verificar email unico
+            // let usuario = await Usuario.findOne({ email: email})
+            const usuario = await Usuario.findOne({ email}) //EcMa Script 6
+            if (usuario){
+                return res.status(400).json({
+                    ok: false,
+                    msg: `Ya existe un usuario con este email: ${email}`
+                });
+            }
+        // Crear usuario con el modelo
+            const dbUser = new Usuario( req.body );
+
+        // Hashear la contraseña
+
+        //Generar el JSON Web Token
+
+        //Crear Usuario en BD
+            await dbUser.save();
+
+        //Generar respuesta 
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Algo salio mal, comuniqiese con el administrador'
+        });
+    }
 }
 
 const loginUsuario = (req,res = response) => {    
