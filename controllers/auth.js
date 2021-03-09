8d2d2155b21b759b045d1f5936cc83ff319cfe0a
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-08 18:00:30 -0600

    middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,6 @@
 const{ response } = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req,res = response) => { //response lo usaremos como un tipado, es comp`letamente opcional
-
-    const errors = validationResult( req );
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
     console.log( req.body );
     const {email, name, password} = req.body; //forma de capturar el body
     console.log(email, name, password);
@@ -23,16 +12,6 @@ const crearUsuario = (req,res = response) => { //response lo usaremos como un ti
 }
 
 const loginUsuario = (req,res = response) => {    
-
-    const errors = validationResult( req );
-    // console.log(errors);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     const {email, password} = req.body;
     console.log(email, password);
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -8,14 +9,15 @@ const router = Router();
 router.post('/new', [
     check('name', 'Nombre invalido').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').isLength({min: 6})
+    check('password', 'El password es obligatorio').isLength({min: 6}),
+    validarCampos
 ],crearUsuario)
 
 // - - - LOGIN DE USUARIO - - -
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').isLength({min: 6})
-    
+    check('password', 'El password es obligatorio').isLength({min: 6}),
+    validarCampos
 ], loginUsuario)
 
 // - - - VALIDAR Y REVALIDAR EL TOKEN - - -
